7e232a200b3b040b3cd1d4ab42baa55688c20a76
Mark <17348049+sockcrates@users.noreply.github.com>
2025-08-28 08:25:57 -0400

    Pin wezterm version

--- flake.nix
+++ flake.nix
@@ -1,14 +1,28 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    oldwezterm.url = "nixpkgs/5ad9903c16126a7d949101687af0aa589b1d7d3d";
   };
 
-  outputs = { self, nixpkgs }:
-    {
+  outputs = { self, nixpkgs, oldwezterm, ... }:
+    let
+      system = "x86_64-linux";
+    in {
       nixosConfigurations = {
         desktop = nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-          modules = [ ./configuration.nix ];
+          inherit system;
+          modules = [
+            ./configuration.nix
+
+            ({ pkgs, ... }: {
+              nixpkgs.overlays = [
+                (final: prev: {
+                  wezterm = oldwezterm.legacyPackages.${final.system}.wezterm;
+                })
+              ];
+            })
+          ];
         };
       };
     };
